f50ccfbdae44331f897bde0a7328ef83978e6f13
AXHyzer <axhyzer@gmail.com>
2026-06-12 20:25:42 -0600

    Estructura de carpetas y nueva libreria para navegar

--- Papeleria/src/App.jsx
+++ Papeleria/src/App.jsx
@@ -1,65 +1,26 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Inicio from './views/Inicio'
+import Productos from './views/Productos'
+import Sucursales from './views/Sucursales'
+import Ofertas from './views/Ofertas'
+import Servicio from './views/Servicio'
+import Admin from './views/Admin'
 
 function App() {
-  const categorias = ['Cuadernos', 'Lapices', 'Pegamento', 'Tijeras', 'Mochilas']
-  const productos = [1, 2, 3, 4, 5, 6]
-
   return (
-    <>
-      <div className="wf-box header">
-        <div className="wf-box logo">LOGO</div>
-        <div className="nav">
-          <div className="wf-box nav-item">Inicio</div>
-          <div className="wf-box nav-item">Productos</div>
-          <div className="wf-box nav-item">Ofertas</div>
-          <div className="wf-box nav-item">Contacto</div>
-        </div>
-        <div className="wf-box nav-item">Carrito</div>
-      </div>
-
-      <div className="wf-box search-bar">Buscar productos...</div>
-
-      <div className="wf-box banner">Banner promocional</div>
-
-      <div className="main-content">
-        <div className="wf-box sidebar">
-          <span className="wf-label">Categorias</span>
-          {categorias.map((cat) => (
-            <div className="wf-box sidebar-item" key={cat}>{cat}</div>
-          ))}
-        </div>
-
-        <div className="products">
-          <div className="product-grid">
-            {productos.map((n) => (
-              <div className="wf-box product-card" key={n}>
-                <div className="product-img"></div>
-                <div className="product-name"></div>
-                <div className="product-price"></div>
-                <button className="btn">Agregar</button>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="wf-box cart">
-          <span className="wf-label">Carrito</span>
-          <div className="wf-box cart-item"></div>
-          <div className="wf-box cart-item"></div>
-          <div className="wf-box cart-item"></div>
-          <button className="btn">Pagar</button>
-        </div>
-      </div>
-
-      <div className="wf-box footer">
-        <span className="wf-label">Footer</span>
-        <div className="footer-links">
-          <div className="wf-box footer-link">Info</div>
-          <div className="wf-box footer-link">FAQ</div>
-          <div className="wf-box footer-link">Redes</div>
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/sucursales" element={<Sucursales />} />
+        <Route path="/ofertas" element={<Ofertas />} />
+        <Route path="/servicio" element={<Servicio />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Papeleria/src/views/Inicio.jsx
@@ -0,0 +1,9 @@
+import heroImg from '../assets/hero.png'
+
+export default function Inicio() {
+  return (
+    <div className="banner-heroe">
+      <img src={heroImg} alt="Papeleria Pavia" />
+    </div>
+  )
+}
